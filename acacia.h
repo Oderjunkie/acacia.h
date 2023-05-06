565d59fa3684e1901d8b286d54f18072f9a9f71a
oats <58880677+Oderjunkie@users.noreply.github.com>
2023-05-06 21:59:03 +0300

    windows support????

--- acacia.h
+++ acacia.h
@@ -1,11 +1,23 @@
 #ifndef OTESUNKI_ACACIA_H
 #define OTESUNKI_ACACIA_H
 #define OTESUNKI_ACACIA_VER 0,1,0
+
+#ifdef _WIN32
+  #ifndef WIN32_LEAN_AND_MEAN
+  #define WIN32_LEAN_AND_MEAN
+  #endif
+  #include <windows.h>
+  #include <winsock2.h>
+  #include <ws2tcpip.h>
+  #include <iphlpapi.h>
+#else
   #include <unistd.h>
+  #include <arpa/inet.h>
+#endif
+
   #include <stdio.h>
   #include <stdlib.h>
   #include <setjmp.h>
-  #include <arpa/inet.h>
   
   extern FILE *netin;
   extern FILE *netout;
@@ -17,8 +29,12 @@
   };
   
   struct ConnectionAcacia {
+#ifdef _WIN32
+    SOCKET ListenSocket, ClientSocket;
+#else
     int sock;
     struct sockaddr_in host_addr;
+#endif
     enum HttpMethodAcacia method;
     char *path;
   };
@@ -32,9 +48,11 @@
   FILE *netin = NULL;
   FILE *netout = NULL;
   
+  #ifndef _WIN32
   static int netfd = -1;
   static char *netbuf = NULL;
   static size_t netlen = 0;
+  #endif
   
   static inline int matchrequest_acacia(
     enum HttpMethodAcacia method,
@@ -64,6 +82,53 @@
   }
   
   static inline void getnextrequest_acacia(struct ConnectionAcacia *connection) {
+#ifdef _WIN32
+    connection->ClientSocket = INVALID_SOCKET;
+    
+    if ((connection->ClientSocket = accept(ListenSocket, NULL, NULL)) == INVALID_SOCKET)
+      fprintf(stderr, "\x1b[31m[ACACIA x00] Failed to accept incoming connection! (WSA: %ld)\x1b[0m\n", WSAGetLastError()),
+      closesocket(connection->ListenSocket),
+      WSACleanup(),
+      exit(EXIT_FAILURE);
+    
+    if ((netin = tmpfile()) == NULL)
+      fprintf(stderr, "\x1b[31m[ACACIA x10] Failed to open temporary file descriptor for netin! (WSA: %ld)\x1b[0m\n", WSAGetLastError()),
+      closesocket(connection->ListenSocket),
+      closesocket(connection->ClientSocket),
+      WSACleanup(),
+      exit(EXIT_FAILURE);
+    
+    if ((netout = tmpfile()) == NULL)
+      fprintf(stderr, "\x1b[31m[ACACIA x11] Failed to open temporary file descriptor for netout! (WSA: %ld)\x1b[0m\n", WSAGetLastError()),
+      closesocket(connection->ListenSocket),
+      closesocket(connection->ClientSocket),
+      WSACleanup(),
+      (void) _rmtmp(),
+      exit(EXIT_FAILURE);
+    
+    {
+      char netinbuf[1024];
+      size_t netinlen = 1024;
+      int IResult;
+      
+      for (;;) {
+        IResult = recv(connection->ClientSocket, netinbuf, netinlen, 0);
+        if (IResult == 0)
+          break;
+        if (IResult < 0)
+          fprintf(stderr, "\x1b[31m[ACACIA x12] Failed to read from socket! (WSA: %ld)\x1b[0m\n", WSAGetLastError()),
+          closesocket(connection->ListenSocket),
+          closesocket(connection->ClientSocket),
+          WSACleanup(),
+          (void) _rmtmp(),
+          exit(EXIT_FAILURE);
+        
+        fwrite(netinbuf, 1, IResult, netin); // TODO: handle possible error here.
+      }
+    }
+    
+    fseek(netin, SEEK_SET, 0); // TODO: handle possible error here.
+#else
     static int netinfd = -1;
     static socklen_t size = sizeof(connection->host_addr);
     
@@ -82,6 +147,7 @@
     if ((netout = open_memstream(&netbuf, &netlen)) == NULL)
       fprintf(stderr, "\x1b[31m[ACACIA x07] Failed to turn buffer into file descriptor! (%s)\x1b[0m\n", strerror(errno)),
       exit(EXIT_FAILURE);
+#endif
   }
   
   static inline void initresponse_acacia(struct ConnectionAcacia *connection) {
@@ -97,7 +163,7 @@
       exit(EXIT_FAILURE);
     
     if (sscanf(buf, "%32s %128s HTTP/%d.%d", reqmth, reqpth, &majver, &minver) == 0)
-      fprintf(stderr, "\x1b[31m[ACACIA x0c] Failed to parse browser's request! (%s)\x1b[0m\n", strerror(errno)),
+      fprintf(stderr, "\x1b[31m[ACACIA x0d] Failed to parse browser's request! (%s)\x1b[0m\n", strerror(errno)),
       exit(EXIT_FAILURE);
     
     if (!strcmp(reqmth, "GET"))
@@ -108,6 +174,38 @@
   }
   
   static inline void sendresponse_acacia(struct ConnectionAcacia *connection) {
+#ifdef _WIN32
+    fseek(netout, SEEK_SET, 0);
+    
+    {
+      char netoutbuf[1024];
+      size_t netoutlen = 1024, bytes_read = 0;
+      for (;;) {
+        bytes_read = fread(netoutbuf, 1, netoutlen, netout);
+        if (bytes_read < netoutlen)
+          if (ferror(netout))
+            fprintf(stderr, "\x1b[31m[ACACIA x14] Failed to read from temporary `netout` buffer! (%s)\x1b[0m\n", strerror(errno)),
+            closesocket(connection->ListenSocket),
+            closesocket(connection->ClientSocket),
+            WSACleanup(),
+            (void) _rmtmp(),
+            exit(EXIT_FAILURE);
+        
+        int IResult;
+        IResult = send(connection->ClientSocket, netinbuf, bytes_read, 0);
+        if (IResult == SOCKET_ERROR)
+          fprintf(stderr, "\x1b[31m[ACACIA x13] Failed to send data to socket! (WSA: %ld)\x1b[0m\n", WSAGetLastError()),
+          closesocket(connection->ListenSocket),
+          closesocket(connection->ClientSocket),
+          WSACleanup(),
+          (void) _rmtmp(),
+          exit(EXIT_FAILURE);
+      }
+    }
+  
+  WSACleanup();
+  (void) _rmtmp();
+#else
     FILE *rawnetout;
     
     if (fclose(netout) == EOF)
@@ -133,14 +231,56 @@
     if (fclose(rawnetout) == -1)
       fprintf(stderr, "\x1b[31m[ACACIA x0a] Failed to close file! (file = %p, %s)\x1b[0m\n", (void *) rawnetout, strerror(errno)),
       exit(EXIT_FAILURE);
+    
+    free(netbuf);
+#endif
   
     if (connection->path)
       free(connection->path);
-    free(netbuf);
+
     exit(EXIT_SUCCESS);
   }
   
   static inline struct ConnectionAcacia *port2con_acacia(struct ConnectionAcacia *connection, unsigned short port) {
+#ifdef _WIN32
+    if (WSAStartup(MAKEWORD(2,2), &wsaData) != 0)
+      fprintf(stderr, "\x1b[31m[ACACIA x0e] Failed to WSAStartup! (WSA: %s)\x1b[0m\n", WSAGetLastError()),
+      exit(EXIT_FAILURE);
+    
+    struct addrinfo *result = NULL, *ptr = NULL, hints;
+    
+    ZeroMemory(&hints, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+    hints.ai_flags = AI_PASSIVE;
+    
+    if (getaddrinfo(NULL, DEFAULT_PORT, &hints, &result) != 0) 
+      fprintf(stderr, "\x1b[31m[ACACIA x0f] Failed to get address info! (WSA: %ld)\x1b[0m\n", WSAGetLastError()),
+      WSACleanup(),
+      exit(EXIT_FAILURE);
+    
+    connection->ListenSocket = INVALID_SOCKET;
+    
+    if ((connection->ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol)) == INVALID_SOCKET)
+      fprintf(stderr, "\x1b[31m[ACACIA x04] Failed to open socket! (WSA: %ld)\x1b[0m\n", WSAGetLastError()),
+      WSACleanup(),
+      exit(EXIT_FAILURE);
+    
+    if (bind(connection->ListenSocket, result->ai_addr, (int)result->ai_addrlen) == SOCKET_ERROR)
+      fprintf(stderr, "\x1b[31m[ACACIA x05] Failed to bind socket to host address! (WSA: %ld)\x1b[0m\n", WSAGetLastError()),
+      freeaddrinfo(result),
+      closesocket(connection->ListenSocket),
+      WSACleanup(),
+      exit(EXIT_FAILURE);
+    
+    freeaddrinfo(result);
+    if (listen(connection->ListenSocket, SOMAXCONN) == SOCKET_ERROR)
+      fprintf(stderr, "\x1b[31m[ACACIA x06] Failed to listen to socket! (WSA: %ld)\x1b[0m\n", WSAGetLastError()),
+      closesocket(connection->ListenSocket),
+      WSACleanup(),
+      exit(EXIT_FAILURE);
+#else
     if ((connection->sock = socket(AF_INET, SOCK_STREAM, 0)) == -1)
       fprintf(stderr, "\x1b[31m[ACACIA x04] Failed to open socket! (%s)\x1b[0m\n", strerror(errno)),
       exit(EXIT_FAILURE);
@@ -156,7 +296,8 @@
     if (listen(connection->sock, SOMAXCONN) != 0)
       fprintf(stderr, "\x1b[31m[ACACIA x06] Failed to listen to socket! (%s)\x1b[0m\n", strerror(errno)),
       exit(EXIT_FAILURE);
-  
+#endif
+    
     return connection;
   }
   
